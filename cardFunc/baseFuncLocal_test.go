9bcb778dc4e4f3ff16a23eae7d83dbd5effae294
RockyFu267 <453618045@qq.com>
2025-01-27 16:54:54 +0800

    add web01

--- cardFunc/baseFuncLocal_test.go
+++ cardFunc/baseFuncLocal_test.go
@@ -29,7 +29,13 @@ func Test_ReadConfig(t *testing.T) {
 		log.Println(err)
 		return
 	}
-	fmt.Println(res)
+	// 将结构体转换为JSON字节切片
+	jsonData, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		fmt.Println("转换为JSON失败:", err)
+		return
+	}
+	fmt.Println(string(jsonData))
 
 }
 
@@ -44,6 +50,26 @@ func Test_HandWinRateSimulationDemo01(t *testing.T) {
 		log.Println(err)
 		return
 	}
+}
+
+func Test_HandWinRateSimulationWeb01(t *testing.T) {
+	handConfig, err := ReadConfig("/Users/fuao/Desktop/开发/github/FA-poker/Hand.yaml")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	res, err := HandWinRateSimulationWeb01(handConfig)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// 将结构体转换为JSON字节切片
+	jsonData, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		fmt.Println("转换为JSON失败:", err)
+		return
+	}
+	fmt.Println(string(jsonData))
 }
 
 func Test_HandWinRateSimulationDemo02(t *testing.T) {
--- cardFunc/type.go
+++ cardFunc/type.go
@@ -17,7 +17,7 @@ type CardRank struct {
 // HanCard 手牌
 type HandCard struct {
 	HandCard [2]Card `json:"handCard"`
-	Advance  Advance `json:"advance"`
+	Advance  Advance `json:"advance,omitempty"`
 	PlayerID string  `json:"playerid,omitempty"`
 }
 
@@ -41,26 +41,26 @@ type TableSingle struct {
 type Players struct {
 	ID           string   `json:"id"`
 	Hand         HandCard `json:"hand"`
-	ChipSum      int64    `json:"chipsum"`
-	ChipBackHand int64    `json:"chipbackhand"`
-	BankRollSum  int64    `json:"bankrollsum"`
-	Card7        [7]Card  `json:"card7"`
-	Card5        [5]Card  `json:"card5"`
-	CardAll      []Card   `json:"cardAll"` //目前默认长度是7
+	ChipSum      int64    `json:"chipsum,omitempty"`
+	ChipBackHand int64    `json:"chipbackhand,omitempty"`
+	BankRollSum  int64    `json:"bankrollsum,omitempty"`
+	Card7        [7]Card  `json:"card7,omitempty"`
+	Card5        [5]Card  `json:"card5,omitempty"`
+	CardAll      []Card   `json:"cardAll,omitempty"` //目前默认长度是7
 	Grade        int      `json:"grade"`
-	TableNum     int      `json:"tablenum"`
-	Sitnum       int      `json:"sitnum"`
-	IsActive     bool     `json:"isactive"`
-	IsFold       bool     `json:"isfold"`
-	IsAllIn      bool     `json:"isallin"`
-	WinCount     int      `json:"winCount"` //单人训练统计次数用的
-	WinRate      float64  `json:"winRate"`  //单人训练统计胜率用的
-	Vpip         float64  `json:"vpip"`     //Voluntarily Put In Pot主动入池率
-	PFR          float64  `json:"pfr"`      //Pre-Flop Raise 翻牌前加注概率
-	FRR          float64  `json:"frr"`      //Flop Raise 翻牌后加注概率
-	TRR          float64  `json:"trr"`      //Turn Raise 转牌后加注概率
-	RRR          float64  `json:"rrr"`      //River Raise 河牌后加注概率
-	ReRaise      float64  `json:"rerraise"` //Re-Raise 加注后加注概率
+	TableNum     int      `json:"tablenum,omitempty"`
+	Sitnum       int      `json:"sitnum,omitempty"`
+	IsActive     bool     `json:"isactive,omitempty"`
+	IsFold       bool     `json:"isfold,omitempty"`
+	IsAllIn      bool     `json:"isallin,omitempty"`
+	WinCount     int      `json:"winCount"`           //单人训练统计次数用的
+	WinRate      float64  `json:"winRate"`            //单人训练统计胜率用的
+	Vpip         float64  `json:"vpip,omitempty"`     //Voluntarily Put In Pot主动入池率
+	PFR          float64  `json:"pfr,omitempty"`      //Pre-Flop Raise 翻牌前加注概率
+	FRR          float64  `json:"frr,omitempty"`      //Flop Raise 翻牌后加注概率
+	TRR          float64  `json:"trr,omitempty"`      //Turn Raise 转牌后加注概率
+	RRR          float64  `json:"rrr,omitempty"`      //River Raise 河牌后加注概率
+	ReRaise      float64  `json:"rerraise,omitempty"` //Re-Raise 加注后加注概率
 }
 
 // RoundHistory	牌局历史
@@ -105,15 +105,22 @@ type PublicCard struct {
 }
 
 type PracticeResDemo02 struct {
-	PlayerWinCount []Players        `json:"playerwincount"` //统计玩家的获胜次数  按座次排序
-	WinGradeList   []WinGradeList   `json:"wingradelist"`   //获胜的成牌牌力分布统计  按出现次数排序
-	DrawCount      int              `json:"drawcount"`      //平局次数
-	So169Combo     []So169ComboList `json:"so169combo"`     //169组so组合的胜率统计  按胜率排序
+	PlayerWinCount []PlayersRes   `json:"playerwincount"`       //统计玩家的获胜次数  按座次排序
+	WinGradeList   []WinGradeList `json:"wingradelist"`         //获胜的成牌牌力分布统计  按出现次数排序
+	DrawCount      int            `json:"drawcount"`            //平局次数
+	So169ComboList []So169Combo   `json:"so169combo,omitempty"` //169组so组合的胜率统计  按胜率排序
+	RoundNumber    int            `json:"roundnumber,omitempty"`
 }
 
-type So169ComboList struct {
+type PlayersRes struct {
+	PlayerID string  `json:"playerid"`
+	WinCount int     `json:"wincount"`
+	WinRate  float64 `json:"winrate"`
+}
+
+type So169Combo struct {
 	WinRateRank int     `json:"winraterank"`
-	So169Combo  string  `json:"so169combo"`
+	So169       string  `json:"so169"`
 	WinRate     float64 `json:"winrate"`
 	ExistCount  int     `json:"existcount"`
 	WinCount    int     `json:"wincount"`
